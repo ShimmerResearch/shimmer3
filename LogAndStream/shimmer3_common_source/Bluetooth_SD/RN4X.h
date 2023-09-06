8fa04982b70877d88a40aaa12c342774c862bd34
Mark Nolan <mnolan@shimmersensing.com>
2023-09-06 16:12:49 +0100

    disabling BLE again after release

--- LogAndStream/shimmer3_common_source/Bluetooth_SD/RN4X.h
+++ LogAndStream/shimmer3_common_source/Bluetooth_SD/RN4X.h
@@ -6,7 +6,7 @@
 /* Utilsed while harmonising LogAndStream and SDLog code bases. This separates the code that is implemented in one FW and not the other. */
 #define FW_IS_LOGANDSTREAM 1
 /* Enables BLE if FW is LogAndStream and the RN4678 is detected */
-#define BT_ENABLE_BLE_FOR_LOGANDSTREAM_AND_RN4678 1
+#define BT_ENABLE_BLE_FOR_LOGANDSTREAM_AND_RN4678 0
 
 /********** Defines created when testing Bluetooth driver - Start ***********/
 /* This toggles the CTS pin whenever the DMA interrupt is being processed to tell the BT module to stop sending bytes while the MCU is not in a position to recieve them */
