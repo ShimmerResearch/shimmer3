94407c3699f1c9034dfbbbb26bc1db345bd38f7b
Mark Nolan <mnolan@shimmersensing.com>
2025-09-24 13:24:00 +0100

    up-rev L&S to v1.00.006

--- LogAndStream/Shimmer_Driver/version.h
+++ LogAndStream/Shimmer_Driver/version.h
@@ -2,10 +2,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define FW_VERSION_MAJOR  1 //16-bit
-#define FW_VERSION_MINOR  0 //8-bit
-#define FW_VERSION_PATCH  5 //8-bit
-#define FW_VERSION_STRING "v1.00.005"
+#define FW_VERSION_MAJOR  1  //16-bit
+#define FW_VERSION_MINOR  0  //8-bit
+#define FW_VERSION_PATCH  6 //8-bit
+#define FW_VERSION_STRING "v1.00.006"
 
 #include <stdint.h>
 
