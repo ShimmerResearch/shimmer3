ceb281834e2c3303a97d162b49592f379cde243f
Mark Nolan <mnolan@shimmersensing.com>
2026-01-29 09:12:44 +0000

    DEV-638 #time 10m new BT command for Shimmer feature setting

--- LogAndStream/Shimmer_Driver/RN4X/RN4678.h
+++ LogAndStream/Shimmer_Driver/RN4X/RN4678.h
@@ -10,8 +10,11 @@
 
 #include <stdint.h>
 
+void RN4678_driverInit(void);
 void RN4678_resetStatusString(void);
 void RN4678_startOfNewStatusString(void);
 uint8_t RN4678_parseStatusString(uint8_t *waitingForArgs, uint8_t *btRxBuffPtr);
+uint8_t RN4678_isErrorLedsEnabled(void);
+void RN4678_setErrorLedsEnabled(uint8_t state);
 
 #endif /* SHIMMER_DRIVER_RN4X_RN4678_H_ */
